722dfffc2eea49642130b1f0585cfcf9ac57269c
Hakim <me@hakim.ninja>
2020-06-07 21:37:17 +0200

    Updates

---
+++ precache-manifest.89d780e63adb592bcb35476c4c826c19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "597a75a340207c94098fea10479070e7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b01efb47442185aa2a8e",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "230662000cb061dece76",
+    "url": "./static/js/2.04c46e58.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.04c46e58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b01efb47442185aa2a8e",
+    "url": "./static/js/main.9e69a70c.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.9e69a70c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aefde1d7571100f300bf",
+    "url": "./static/js/runtime-main.175e6ea3.js"
+  }
+]);
